a367a8314a2517d62f40e4b6143d4d5824ac5d4a
Lyle Simmons <lylesimmons125@gmail.com>
2024-01-31 21:39:23 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+//findMatching
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+const findMatching = (array, name) => array.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
+//fuzzyMatch
+const fuzzyMatch = (array, letter) => array.filter(driver => driver.startsWith(letter));
+
+//matchName
+
+const matchName = (array, name) => array.filter(obj => obj.name === name);
